bd9c984320ef18a963c7227fe514302fe15d9dc5
Ogi <ostamenkovic14@gmail.com>
2021-04-11 13:48:21 +0200

    Added background and main menu buttons.

--- frontend/src/home_screen.js
+++ frontend/src/home_screen.js
@@ -1,18 +1,42 @@
-export const screen = null;
-export const nameWindow = null;
+let screen = null;
+let nameWindow = null;
 
 export const initHomeScreen = () => {
+
+    screen = document.createElement('div');
+    screen.id = 'homeScreen';
+
+    let buttonStart = document.createElement('div');
+    buttonStart.id = 'btnStart';
+    buttonStart.onclick = () => {
+        //TODO
+    }
+
+    let buttonExit = document.createElement('div');
+    buttonExit.id = 'btnExit';
+    buttonExit.onclick = () => {
+        //TODO
+    }
+
+    screen.appendChild(buttonStart);
+    screen.appendChild(buttonExit);
 
 }
 
 export const setNameWindow = () => {
 
+
+
 }
 
 export const postHomeScreen = () => {
 
+    document.body.appendChild(screen);
+
 }
 
 export const deleteHomeScreen = () => {
+
+    document.body.removeChild(home);
 
 }
