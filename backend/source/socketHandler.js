7aa19d1ec15ce2b876918075005156c515ea217d
phalto <tomislavsavatijevic@gmail.com>
2021-04-18 16:58:24 +0200

    Implemented features for socketHandler

--- backend/source/socketHandler.js
+++ backend/source/socketHandler.js
@@ -1,5 +1,104 @@
+const Card = require('../models/card');
+const Game = require('../models/game');
+
+let gamesCounter = 0;
+
+let allGames = [];
+let gameIndex = {};
+let rematch = {};
+let accept = {};
+let playersNames = {};
+lobby = '';
+
+let gameDuration = 48;
+
+
 socketHandler = (socket) => {
     console.log('Established connection: ' + socket.id);
+
+    socket.emit('successful', {
+        id: socket.id,
+    });
+
+    socket.on('gameLobby', (msg) => {
+        
+        playersNames[socket.id] = msg.name
+        if (lobby === '') {
+            lobby = socket.id;
+        }
+        else {
+            const game = new Game(socket.id, lobby);
+
+            game.deckOfCards.shuffle();
+            
+            allGames.push(game);
+            gameIndex[lobby] = gamesCounter;
+            gameIndex[socket.id] = gamesCounter;
+
+            socket.emit('ready', {
+                opponentName: playersNames[lobby],
+                playerName: playersNames[socket.id]
+            });
+
+            socket.to(lobby).emit('ready', {
+                opponentName: playersNames[socket.id],
+                playerName: playersNames[lobby]
+            });
+
+            lobby = '';
+
+            gamesCounter += 1;
+        }
+        
+       
+    })
+
+    socket.on('gameStart', (msg) => {
+        const index = gameIndex[socket.id];
+        const game = allGames[index];
+        const num = socket.id == game.playersId.player1 ? 1 : 2;
+        const turn = game.turn;
+        const player = 'player' + num;
+
+        socket.emit('turnOn', {
+            action: 'deal',
+            hand: game.playersHand[player].toStringArray(),
+            turn: num == turn,
+            turnNum: game.turnCounter
+        });
+
+    });
+
+    socket.on('turn', (res) => {
+     // TODO
+
+    });
+
+    socket.on('disconnect', () => {
+        console.log('disconnected: ' + socket.id)
+
+        const index = gameIndex[socket.id];
+
+        if (index >= 0) {
+            const game = allGames[index];
+            const oppId = socket.id == game.playersId.player1
+                ? game.playersId.player2 : game.playersId.player1;
+
+            socket.to(oppId).emit('end', {
+                msg: 'disconnected'
+            });
+
+            game.setTurnCounter(-1);
+
+            delete gameIndex[socket.id];
+            delete gameIndex[oppId];
+        }
+
+        delete playersNames[socket.id];
+        
+    });
+
+
 }
 
 module.exports = socketHandler;
