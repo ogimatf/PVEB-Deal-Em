45e1ea9e54292612e66630a8a9c6976bd91bbb3f
v1rtuoso <mutavdzic.djordje12@gmail.com>
2021-06-02 15:22:53 +0200

    feat: Init database

--- backend/source/app.js
+++ backend/source/app.js
@@ -1,20 +1,45 @@
-const handler = require('./socketHandler')
-const app = require('express')();
-const http = require('http').createServer(app);
+const handler = require("./socketHandler");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const routes = require("./routes");
+const cors = require("cors");
+const db = require("./db");
+
+db.createConnection();
+
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
+
+app.use("/user", routes);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  const error = new Error("Method not alowed");
+  error.status = 405;
 
-  next();
+  next(error);
 });
 
-io.on('connection', handler);
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      msg: error.message,
+    },
+  });
+});
 
-module.exports = http
+io.on("connection", handler);
+
+module.exports = http;
