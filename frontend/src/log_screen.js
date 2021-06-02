ea7d57b1a7fe4de942bafbeb22eff34b926c01a8
v1rtuoso <mutavdzic.djordje12@gmail.com>
2021-06-02 15:34:08 +0200

    feat: Utility functions

--- frontend/src/log_screen.js
+++ frontend/src/log_screen.js
@@ -1,41 +1,119 @@
-import * as home_screen from './home_screen.js';
+import { setIsLoged, setPlayer1Name } from "./game_state.js";
+import * as home_screen from "./home_screen.js";
 
 let screen = null;
 
+let blur;
+let registWin;
+let title;
+let form;
+
 export const initLogScreen = () => {
+  screen = document.createElement("div");
+  screen.id = "logScreen";
 
-    screen = document.createElement('div');
-    screen.id = 'logScreen';
+  let buttonLogovanje = document.createElement("div");
+  buttonLogovanje.id = "btnLogovanje";
+  buttonLogovanje.onclick = () => {
+    setWindow();
+    logUser();
+  };
 
-    let buttonLogovanje = document.createElement('div');
-    buttonLogovanje.id = 'btnLogovanje';
-    buttonLogovanje.onclick = () => {
-        //TODO
-    }
+  let buttonRegist = document.createElement("div");
+  buttonRegist.id = "btnRegist";
+  buttonRegist.onclick = () => {
+    setWindow();
+    registUser();
+  };
 
-    let buttonRegist = document.createElement('div');
-    buttonRegist.id = 'btnRegist';
-    buttonRegist.onclick = () => {
-        //TODO
-    }
+  let buttonBack = document.createElement("div");
+  buttonBack.id = "btnLogBack";
+  buttonBack.onclick = () => {
+    document.body.removeChild(screen);
+    home_screen.postHomeScreen();
+  };
 
-    let buttonBack = document.createElement('div');
-    buttonBack.id = 'btnLogBack';
-    buttonBack.onclick = () => {
-        document.body.removeChild(screen);
-        home_screen.postHomeScreen();
-    }
+  screen.appendChild(buttonBack);
+  screen.appendChild(buttonLogovanje);
+  screen.appendChild(buttonRegist);
+};
 
-    screen.appendChild(buttonBack);
-    screen.appendChild(buttonLogovanje);
-    screen.appendChild(buttonRegist);
-}
+export const postRegistScreen = () => {
+  document.body.appendChild(screen);
+};
 
+export const deleteRegistScreen = () => {
+  document.body.removeChild(regist);
+};
 
-export const postLogScreen = () => {
-    document.body.appendChild(screen);
-}
+export const setWindow = () => {
+//TODO
+};
 
-export const deleteLogScreen = () => {
-    document.body.removeChild(log);
-}
+const registUser = () => {
+//TODO
+};
+
+const registRequest = async () => {
+//TODO
+};
+
+const removeRegistWindow = () => {
+//TODO
+};
+
+export const setRegistWindow = () => {
+//TODO
+};
+
+const logUser = () => {
+//TODO
+};
+
+const logRequest = async () => {
+//TODO
+};
+
+const removeLogWindow = () => {
+//TODO
+};
+
+export let errorMsg = "";
+
+export const setErrorMsg = (msg) => {
+  errorMsg = msg;
+};
+
+export const writeErrorMsg = () => {
+  let msgBar = document.getElementById("msg-bar");
+  msgBar.innerHTML = errorMsg;
+};
+
+export const getInputValue = (elementName, minlength, maxlength) => {
+  let value = document.getElementsByName(elementName)[0].value;
+  let element = document.getElementsByName(elementName)[0];
+
+  if (minlength && value == "") {
+    errorMsg += elementName + " is required.<br>";
+    element.style.borderColor = "red";
+    writeErrorMsg();
+  } else if (minlength && value.length < minlength) {
+    errorMsg +=
+      elementName + " should contains " + minlength + " or more characters<br>";
+    element.style.borderColor = "red";
+    writeErrorMsg();
+  } else if (maxlength && value.length > maxlength) {
+    errorMsg +=
+      elementName +
+      " should not contains more then<br>" +
+      maxlength +
+      " characters<br>";
+    element.style.borderColor = "red";
+    writeErrorMsg();
+  } else if (value != "") {
+    element.style.borderColor = "#EBE9ED";
+    return value;
+  }
+
+  return null;
+};
